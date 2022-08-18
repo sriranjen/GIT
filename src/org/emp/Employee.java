908caf9e102c5cdb782804fa7dd672ef72e0fc8d
sriranjen <sriranjen@gmail.com>
2022-08-18 17:48:31 +0530

    my first project

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,37 @@
+package org.emp;
+
+public class Employee {
+	private void empId() {
+		System.out.println("employee id is : 1234");
+
+	}
+
+	private void empId(int a) {
+		System.out.println("employee id is : 1230");
+
+	}
+
+	private void empId(float a) {
+		System.out.println("employee id is : 1231");
+
+	}
+
+	private void empId(char a) {
+		System.out.println("employee id is : 1232");
+
+	}
+
+	private void empId(String a) {
+		System.out.println("employee id is : 1235");
+
+	}
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.empId();
+		e.empId(123456);
+		e.empId(12.234f);
+		e.empId('s');
+		e.empId("string");
+	}
+}
